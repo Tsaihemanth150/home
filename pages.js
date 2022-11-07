49a1c55e5e44fad2b4264e2aa115446c51d5c084
Tsaihemanth150 <90019941+Tsaihemanth150@users.noreply.github.com>
2022-11-07 16:01:45 +0530

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/Tsaihemanth150/Tsaihemanth150.github.io.git";
 
 ghpages.publish(
   pathname,
